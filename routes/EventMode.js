a472cbcfd58a6fafc7afa0378639cdb7ca42910d
Erin Shaben <eshaben@icloud.com>
2017-10-25 14:49:06 -0600

    got rid of extra folder from merge

--- routes/EventMode.js
+++ routes/EventMode.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {Alert, Image, Text, TouchableOpacity, View, AsyncStorage, TextInput} from 'react-native';
 import {Actions} from 'react-native-router-flux';
+import { Card, ListItem, Button } from 'react-native-elements'
 import styles from './styles';
 import jwt_decode from 'jwt-decode';
+
 
 class EventMode extends Component {
 
@@ -14,8 +16,23 @@ class EventMode extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}> What would you like to accomplish? </Text>
-
+        <Text style={styles.title}> Event Mode </Text>
+        <Card title="CARD WITH DIVIDER">
+            {
+              users.map((u, i) => {
+                return (
+                  <View key={i} style={styles.user}>
+                    <Image
+                      style={styles.image}
+                      resizeMode="cover"
+                      source={{ uri: u.avatar }}
+                    />
+                    <Text style={styles.name}>{u.name}</Text>
+                  </View>
+                );
+              })
+            }
+          </Card>
 
           <TouchableOpacity style={styles.buttonWrapper} onPress={Actions.HomePage}>
             <Text style={styles.buttonText}> Go Back </Text>
